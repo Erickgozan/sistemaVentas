687449ef877f8f122215d43599dae84c09db6110
Erick Gonzalez <erickgozan@gmail.com>
2020-01-12 15:35:48 -0600

    Se creo el metodo listarProductos

--- src/main/java/com/tianguisweb/controlador/ControladorIMG.java
+++ src/main/java/com/tianguisweb/controlador/ControladorIMG.java
@@ -5,79 +5,62 @@
  */
 package com.tianguisweb.controlador;
 
+import com.tianguisweb.modelo.ProductoDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
-/**
- *
- * @author erick
- */
 public class ControladorIMG extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ControladorIMG</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ControladorIMG at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+     @Resource(name = "jdbc/tianguisWeb")
+    private DataSource conexion;
+    private ProductoDAO productoDao;
+
+   @Override
+    public void init() throws ServletException {
+        try {
+            productoDao = new ProductoDAO(conexion);
+        } catch (Exception e) {
+            System.out.println("Eroror: " + e.getLocalizedMessage());
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+         try {
+             response.setContentType("text/html;charset=UTF-8");
+             int idProducto = Integer.parseInt(request.getParameter("id"));
+             productoDao.listarImg(idProducto, response); 
+             //response.getWriter().print("funciona");
+         } catch (SQLException ex) {
+             Logger.getLogger(ControladorIMG.class.getName()).log(Level.SEVERE, null, ex);
+         }
+    }
+
+   
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+  
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
