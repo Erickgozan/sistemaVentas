e5881e7fdb2405d130a5508f1dcf7b31ca3f0d9b
Erick Gonzalez <erickgozan@gmail.com>
2020-01-12 00:54:16 -0600

    Se contruyó la clase Producto con sus geters y seters

--- src/main/java/com/tianguisweb/modelo/Producto.java
+++ src/main/java/com/tianguisweb/modelo/Producto.java
@@ -1,14 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.tianguisweb.modelo;
 
-/**
- *
- * @author erick
- */
+import java.io.InputStream;
+
 public class Producto {
+    
+    private String nombre;
+    private InputStream imagen;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    
+    public Producto(){}
+
+    public Producto(String nombre, InputStream imagen, String descripcion, double precio, int stock) {
+   
+        this.nombre = nombre;
+        this.imagen = imagen;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public InputStream getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(InputStream imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
+    
+    
     
 }
