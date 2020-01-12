25ac06806e8ad59ffcf5b39434a91bb3e11c43a4
Erick Gonzalez <erickgozan@gmail.com>
2020-01-12 15:36:43 -0600

    Se creo el metodo listarProductos

--- src/main/java/com/tianguisweb/controlador/Controlador.java
+++ src/main/java/com/tianguisweb/controlador/Controlador.java
@@ -10,6 +10,11 @@ import com.tianguisweb.modelo.ProductoDAO;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -23,13 +28,13 @@ import javax.sql.DataSource;
 public class Controlador extends HttpServlet {
 
     @Resource(name = "jdbc/tianguisWeb")
-    private DataSource pool;
+    private DataSource conexion;
     private ProductoDAO productoDao;
 
     @Override
     public void init() throws ServletException {
         try {
-            productoDao = new ProductoDAO(pool);
+            productoDao = new ProductoDAO(conexion);
         } catch (Exception e) {
             System.out.println("Eroror: " + e.getLocalizedMessage());
         }
@@ -39,13 +44,15 @@ public class Controlador extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
 
-        String metodo = request.getParameter("agregar");
+        String accion = request.getParameter("accion");
 
-        switch (metodo) {
+        switch (accion) {
 
-            case "insertar":
+            case "agrgegar":
                 agregarProducto(request, response);
                 break;
+            default:
+            listarProductos(request,response);
 
         }
     }
@@ -63,7 +70,12 @@ public class Controlador extends HttpServlet {
             double precio = Double.parseDouble(request.getParameter("precio"));
             int stock = Integer.parseInt(request.getParameter("stock"));
 
-            p = new Producto(nombreProducto, isIMG, descripcion, precio, stock);
+            p = new Producto();
+            p.setNombre(nombreProducto);
+            p.setImagen(isIMG);
+            p.setDescripcion(descripcion);
+            p.setPrecio(precio);
+            p.setStock(stock);
 
             productoDao.agregarProducto(p);
 
@@ -74,30 +86,26 @@ public class Controlador extends HttpServlet {
         }
 
     }
+    
+      private void listarProductos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            List<Producto> productos;
+            productos = productoDao.listar();
+            request.setAttribute("productos", productos);
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(Controlador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -113,5 +121,7 @@ public class Controlador extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+  
 
 }
