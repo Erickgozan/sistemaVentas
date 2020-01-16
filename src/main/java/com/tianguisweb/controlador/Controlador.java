c3351b9833db269b787270cc1e5a35dd5cc6d341
Erick Gonzalez <erickgozan@gmail.com>
2020-01-16 00:13:01 -0600

    Se crearon los metodos agregarCarrito y listarCarrito

--- src/main/java/com/tianguisweb/controlador/Controlador.java
+++ src/main/java/com/tianguisweb/controlador/Controlador.java
@@ -5,6 +5,7 @@
  */
 package com.tianguisweb.controlador;
 
+import com.tianguisweb.modelo.Carrito;
 import com.tianguisweb.modelo.Producto;
 import com.tianguisweb.modelo.ProductoDAO;
 import java.io.IOException;
@@ -30,7 +31,12 @@ public class Controlador extends HttpServlet {
     @Resource(name = "jdbc/tianguisWeb")
     private DataSource conexion;
     private ProductoDAO productoDao;
-
+     PrintWriter out;
+     List<Carrito> listaCarrito = new ArrayList<>();
+     int item;
+     double totalPagar = 0.0;
+     int cantidad = 1;
+     
     @Override
     public void init() throws ServletException {
         try {
@@ -51,11 +57,17 @@ public class Controlador extends HttpServlet {
             case "agrgegar":
                 agregarProducto(request, response);
                 break;
+            case "agreagrCarrito":
+                agregarCarrito(request,response);
+                break;
+            case "carrito":
+                listarCarrito(request,response);
+                break;
             case "home":
-              listarProductos(request,response);
-              break;
+                listarProductos(request, response);
+                break;
             default:
-            listarProductos(request,response);
+                listarProductos(request, response);
 
         }
     }
@@ -63,7 +75,7 @@ public class Controlador extends HttpServlet {
     private void agregarProducto(HttpServletRequest request, HttpServletResponse response) {
 
         try {
-            PrintWriter out = response.getWriter();
+           
             Producto p;
 
             String nombreProducto = request.getParameter("nombreProducto");
@@ -85,12 +97,12 @@ public class Controlador extends HttpServlet {
             request.getRequestDispatcher("Controlador?accion=home").forward(request, response);
 
         } catch (IOException | ServletException ex) {
-            System.out.println("Error al agregar el producto " + ex.getLocalizedMessage());
+            out.println("Error al agregar el producto " + ex.getLocalizedMessage());
         }
 
     }
-    
-      private void listarProductos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    private void listarProductos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
             List<Producto> productos;
             productos = productoDao.listar();
@@ -100,15 +112,51 @@ public class Controlador extends HttpServlet {
             Logger.getLogger(Controlador.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
     
+     private void agregarCarrito(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            int idP = Integer.parseInt(request.getParameter("idProducto"));
+            Producto p;
+            p = productoDao.listarId(idP);
+            item+=1;
+            Carrito car = new Carrito();
+            car.setItem(item);
+            car.setIdProducto(p.getIdProducto());
+            car.setNombre(p.getNombre());
+            car.setDescripcion(p.getDescripcion());
+            car.setPrecioCompra(p.getPrecio());
+            car.setCantidad(cantidad);
+            car.setSubTotal(cantidad*p.getPrecio());
+             listaCarrito.add(car);
+             request.setAttribute("contador", listaCarrito.size());
+             request.getRequestDispatcher("Controlador?accion=home").forward(request, response);
+        } catch (SQLException | IOException | ServletException ex) {
+           out.print("Error en el metodo agregarCarrito " + ex.getLocalizedMessage());
+        }
+    }
+     
+     private void listarCarrito(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            totalPagar=0.0;
+            request.setAttribute("carrito", listaCarrito);
+            for (int i = 0; i < listaCarrito.size(); i++) {
+                totalPagar=totalPagar+listaCarrito.get(i).getSubTotal();
+            }
+            request.setAttribute("totalApagar", totalPagar);
+            request.getRequestDispatcher("carrito.jsp").forward(request, response);
+        } catch (IOException | ServletException ex) {
+            out.print("Error en el metodo listarCarriot " + ex.getLocalizedMessage());
+        }
+    }
+
+   
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -125,6 +173,6 @@ public class Controlador extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
-  
+    
 
 }
